0a3ecd03ad0faaa348a52525c602317aeb5dcf04
Ethan Doering <ethanwdoering@icloud.com>
2023-03-24 13:09:23 -0600

    📦 NEW: Solved problemOne and tested it.

--- app.js
+++ app.js
@@ -119,14 +119,13 @@ function filterExample(){
     //Debug tip: Use a console.log(el) inside the filter function to get a visualization of what el represents and see all its properties! This helps you to know what you can access with dot notation inside the filter. Do this every time you use a .filter or else you are working in the dark!
     let results;
     results = dishes.filter(function(el){
-        console.log("el inside filterExample's filter: ", el)
+//        console.log("el inside filterExample's filter: ", el)
         if(el.cuisine === "Mexican"){
             return true;
         }
         else{
             return false;
         }})
-
     return results;
 }
 
@@ -142,10 +141,19 @@ console.log('mexicanFood from filterExample', mexicanFood)
 function problemOne(){
 
     let results;
-
+    results = dishes.filter(function(el){
+        console.log("el inside problemOne's filter ", el)
+        if(el.cuisine == "Vegetarian"){
+            return true;
+        }
+        else{
+            return false;
+        }})
     return results;
 }
 
+let vegetarianFood = problemOne();
+console.log("vegetarianFood from problemOne", vegetarianFood);
 //2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
 //Filter
 
